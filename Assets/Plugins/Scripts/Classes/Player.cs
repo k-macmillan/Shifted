fc439a78bff08944314bf0da56feb2232ca75783
macattackftw <macattackftw@gmail.com>
2018-08-13 01:40:30 -0600

    Added const for the number of randoms to create
    NextDouble is not a separate pool, it generates the double based on the next psuedorandom number. If you do not keep the integers and doubles separate there may be problems with reproducing results, therefore they are separate.

--- Assets/Plugins/Scripts/Classes/Player.cs
+++ Assets/Plugins/Scripts/Classes/Player.cs
@@ -4,35 +4,41 @@ using UnityEngine;
 
 public class Player {
 
-    Int32[] RandomInts { get; set; }
-    double[] RandomDouble { get; set; }
+    private const int RandomIntCount = 5;
+    private const int RandomDoubleCount = 5;
+
+    public Int32[] RandomInts { get; private set; }
+    public double[] RandomDouble { get; private set; }
 
 
     public Player()
     {
-        System.Random rnd = new System.Random(42);
-        System.Random rnd2 = new System.Random(42);
 
-        int qty = 5;
-        RandomInts = new Int32[qty];
-        RandomDouble = new double[qty];
+        RandomInts = new Int32[RandomIntCount];
+        RandomDouble = new double[RandomDoubleCount];
+
+
+        System.Random rnd2 = new System.Random(42);
+        for (int i = 0; i < RandomDoubleCount; ++i)
+        {
+            RandomDouble[i] = rnd2.NextDouble();
+        }
+        System.Random rnd = new System.Random(42);
+        for (int i = 0; i < RandomIntCount; ++i)
+        {
+            RandomInts[i] = rnd.Next();            
+        }
 
         
 
-        for(int i = 0; i < qty; ++i)
-        {
-            RandomInts[i] = rnd.Next();
-            RandomDouble[i] = rnd2.NextDouble();
-        }
+        //for (int i = 0; i < RandomIntCount; ++i)
+        //{
+        //    Debug.Log("INT: " + RandomInts[i] % 1000);
+        //}
 
-        for (int i = 0; i < qty; ++i)
-        {
-            Debug.Log("INT: " + RandomInts[i] % 1000);
-        }
-
-        for (int i = 0; i < qty; ++i)
-        {
-            Debug.Log("DBL: " + RandomDouble[i]);
-        }
+        //for (int i = 0; i < RandomDoubleCount; ++i)
+        //{
+        //    Debug.Log("DBL: " + RandomDouble[i]);
+        //}
     }
 }
