eb58ea92785bc95f8cad11e0d323dbbe066e0b7d
macattackftw <macattackftw@gmail.com>
2018-08-13 02:19:41 -0600

    Changed random pools to be replenishing
    Made it so the int and double pools replenish if they get bled dry. Added a LogWarning so I would be able to see when it happens in case it gets called too many times by accident as I'm still learning.

--- Assets/Plugins/Scripts/Classes/Player.cs
+++ Assets/Plugins/Scripts/Classes/Player.cs
@@ -4,41 +4,74 @@ using UnityEngine;
 
 public class Player {
 
-    private const int RandomIntCount = 5;
-    private const int RandomDoubleCount = 5;
+    private const int RandomIntCount = 50;
+    private const int RandomDoubleCount = 50;
+    private int RndIntIndex = 0;
+    private int RndDblIndex = 0;
+    private System.Random rndInts;
+    private System.Random rndDbls;
 
     public Int32[] RandomInts { get; private set; }
     public double[] RandomDouble { get; private set; }
 
 
+
     public Player()
     {
-
         RandomInts = new Int32[RandomIntCount];
         RandomDouble = new double[RandomDoubleCount];
+        rndInts = new System.Random(42);
+        rndDbls = new System.Random(42);
 
 
-        System.Random rnd2 = new System.Random(42);
         for (int i = 0; i < RandomDoubleCount; ++i)
         {
-            RandomDouble[i] = rnd2.NextDouble();
+            RandomDouble[i] = rndDbls.NextDouble();
         }
-        System.Random rnd = new System.Random(42);
+        
         for (int i = 0; i < RandomIntCount; ++i)
         {
-            RandomInts[i] = rnd.Next();            
+            RandomInts[i] = rndInts.Next();            
         }
 
         
 
-        //for (int i = 0; i < RandomIntCount; ++i)
-        //{
-        //    Debug.Log("INT: " + RandomInts[i] % 1000);
-        //}
+    }
 
-        //for (int i = 0; i < RandomDoubleCount; ++i)
-        //{
-        //    Debug.Log("DBL: " + RandomDouble[i]);
-        //}
+
+    /// <summary>
+    /// Gets a random integer from the buffer. Replenishes pool when it runs out.
+    /// </summary>
+    /// <returns>Random integer</returns>
+    public Int32 GetRandomInt()
+    {
+        if (RndIntIndex == RandomIntCount)
+        {
+            RndIntIndex = 0;
+            for (int i = 0; i < RandomIntCount; ++i)
+            {
+                RandomInts[i] = rndInts.Next();
+            }
+            Debug.LogWarning("REPLENISHED INTEGER POOL");
+        }
+        return RandomInts[RndIntIndex++];
+    }
+
+    /// <summary>
+    /// Gets a random double from the buffer. Replenishes pool when it runs out.
+    /// </summary>
+    /// <returns>Random double</returns>
+    public double GetRandomDouble()
+    {
+        if (RndDblIndex == RandomDoubleCount)
+        {
+            RndDblIndex = 0;
+            for (int i = 0; i < RandomDoubleCount; ++i)
+            {
+                RandomDouble[i] = rndDbls.NextDouble();
+            }
+            Debug.LogWarning("REPLENISHED DOUBLE POOL");
+        }
+        return RandomDouble[RndDblIndex++];
     }
 }
